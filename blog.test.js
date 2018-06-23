e838fc018396145e8ca39342341018eb96baa998
Jean-Marcel Belmont <jbelmont@foodlogiq.com>
2018-06-23 13:19:17 -0400

    Add puppeteer travis and jest test.

---
+++ blog.test.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const puppeteer = require("puppeteer");
+
+let page;
+let browser;
+const webpage = "http://www.marcelbelmont.com/";
+const width = 1440;
+const height = 900;
+const myBlog = "blog.png"
+
+describe("Marcel Belmont Blog", () => {
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    });
+    page = await browser.newPage();
+    await page.setViewport({ width, height });
+  });
+
+  afterAll(async () => {
+    await page.screenshot({path: myBlog});
+    browser.close();
+  });
+
+  it("Should have a biography id with test", async () => {
+    await page.goto(webpage);
+    const textContent = await page.evaluate(() => document.querySelector("#biography").textContent);
+    expect(textContent).toEqual("Biography");
+  });
+});
